37db2ac3d952789c7de37da7a22acc8c3b8ab2b8
marwane <marw0619@stud.kea.dk>
2021-02-07 15:33:20 +0100

    s

---
+++ basic.js
@@ -0,0 +1,29 @@
+"use strict";
+console.log("hahaha");
+
+const boul = true ;
+const num = 41;
+const string = "marva";
+const obj  = {
+    cats : 0,
+    cars:1
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+const value = " " ;
+
+if (value) {
+    console.log("value is thuthy");
+} else {
+    console.log("Value is falsy");  
+}
+
+
