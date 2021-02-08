9bb418dcdc3bde7d22e2edcdb9ffac01b7353178
marwane <marw0619@stud.kea.dk>
2021-02-08 12:37:57 +0100

    the_thrid_ex

--- capitalization.js
+++ capitalization.js
@@ -1,10 +1,17 @@
 const daName = "peter" ;
 
-
 const upp1 = daName.substring(0,3) + daName.toUpperCase()[3] + daName.substring(4);
 const upp2 = daName.substring(0,2) + daName.toUpperCase()[2] +daName.toUpperCase()[3] + daName.substring(4);
-const upp3 =daName.toUpperCase()[0] + daName.toUpperCase()[1] + daName.toUpperCase()[2] +daName.substring(3,4) +daName.toUpperCase()[4] ;
+const upp3 = daName.toUpperCase()[0] + daName.toUpperCase()[1] + daName.toUpperCase()[2] +daName.substring(3,4) +daName.toUpperCase()[4] ;
 
 console.log(upp1);
 console.log(upp2);
 console.log(upp3);
+
+const daOthername = "                marwane  " ;
+
+const clean = daOthername.trim() ;
+
+const capp1 = clean.toUpperCase()[0] + clean.substring(1) ;
+
+console.log(capp1) ;    
