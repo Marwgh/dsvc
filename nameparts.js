b417eae03cd80db3a99e2b2c43dd1c120e9cb1f5
marwane <marw0619@stud.kea.dk>
2021-02-08 11:35:58 +0100

    read_first_midl_last_name

---
+++ nameparts.js
@@ -0,0 +1,20 @@
+
+
+const nameS = "Marwane Benji Ghalila" ;
+
+
+let first = nameS.split(" ");
+first = first[0];
+
+let middle = nameS.split(" ");
+middle = middle[1];
+
+let last = nameS.split(" ");
+last = last[2]
+
+
+
+
+console.log(first);
+console.log(middle);
+console.log(last);
